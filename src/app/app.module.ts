b1cfd2b603b3b6d83aa67c4c93d243ac5f453e86
Vruddhi Mehta <vgmehta_b17@ce.vjti.ac.in>
2020-05-10 06:03:17 +0530

    COmponents Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
- 
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule, MatToolbarModule, FlexLayoutModule,
+    BrowserModule, MatToolbarModule, FlexLayoutModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
