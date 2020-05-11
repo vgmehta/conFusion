e2f88e1210edcad73894aeb6fea81b787d58559f
Vruddhi Mehta <vgmehta_b17@ce.vjti.ac.in>
2020-05-11 19:40:49 +0530

    Menu Component Testing

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -3,6 +3,6 @@ import { HighlightDirective } from './highlight.directive';
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
     const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
+    expect(directive).toBeFalsy();
   });
 });
